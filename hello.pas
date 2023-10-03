7ecd40110f9ca46478665d409c5daa05c1b08e87
Лужняк Александр <Lyzhniak2005@yandex.ru>
2023-10-03 16:03:40 +0300

    Файл Привет мир! на 4х языках.

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var l:= readstring('Enter language(ENG\RUS\UKR\GER):');
+  case l of
+    'RUS': print('Привет мир!');
+    'ENG': print('Hello world!');
+    'UKR': print('Привiт свiт!');
+    'GER': print('Hallo Welt!');
+  end;
+  
+  
+  
+end.
